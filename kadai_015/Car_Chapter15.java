308cf1efcafdc940413802b9543e3d51e7ed86c7
tsyoshida <toshinori.yoshida@riskmonster.co.jp>
2025-03-19 11:59:20 +0900

    Add files via upload

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -11,21 +11,21 @@ public class Car_Chapter15 {
 	//メソッド
 	// ギアチェンジメソッド
 	public void gearChange(final int afterGear) {
-		this.gear = afterGear;
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました。");
+        this.gear = afterGear;
 		switch(afterGear) {
 			case 1:this.speed = 10; break;
 			case 2:this.speed = 20; break;
 			case 3:this.speed = 30; break;
 			case 4:this.speed = 40; break;
 			case 5:this.speed = 50; break;
-			default:this.speed = 10; break; // 1~5以外のギアは10km/h
+			default:this.speed = 10; break; // 1～5以外のギアは10km/h
 		}
-		
 	}
 	
 	// 速度を表示するメソッド
 	public void run() {
-		System.out.println("現在のギア: " + gear + "、速度: " + speed + "km/h");
+		System.out.println("速度は時速"  + speed + "kmです");
 	}
 	
 	
